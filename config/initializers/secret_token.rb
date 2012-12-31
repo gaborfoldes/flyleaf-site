8ece0e666863cb3c40920f1b8bf08b1b568b9ac8
Gabor Foldes <gabor.foldes@gmail.com>
2012-12-30 22:57:45 -0800

    start new rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FlyleafSite::Application.config.secret_token = '9a914e20a1d6d70e59a7fe7be2a6ba9199697c6a258f92366968fbca037604c24bb4fc45454fdbb667f12a5ac3a6ec30e740b6da505ede345e8d4d787a11ff31'
